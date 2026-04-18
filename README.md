8320cda4e84641ff0a8ba3264a6b8360e77bae76
deji2ghost <dejiwilliams9@gmail.com>
2026-04-18 13:46:23 +0100

    feat:updated readme.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# personal-api
+# Personal API
+
+A simple REST API built with Node.js and Express, deployed on AWS EC2 with Nginx as a reverse proxy.
+
+## Run Locally
+
+```bash
+git clone https://github.com/YOURUSERNAME/personal-api.git
+cd personal-api
+npm install
+node index.js
+```
+
+API will be available at `http://localhost:3000`
+
+## Endpoints
+
+ Endpoint       Method          Response 
+
+ `/`              GET            `{"message": "API is running"}` |
+ `/health`        GET              `{"message": "healthy"}` |
+ `/me`            GET              `{"name": "...", "email": "...", "github": "..."}` |
+
+## Live URL
+
+http://13.51.48.0/
--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+const API_KEY = process.env.API_KEY || "test123";
+
+const authMiddleware = (req, res, next) => {
+  const key = req.headers["x-api-key"];
+
+  if (!key || key !== API_KEY) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  next();
+};
+
+// app.use("/", authMiddleware);
+// app.use("/health", authMiddleware);
+// app.use("/me", authMiddleware);
+
 // Endpoint 1: GET /
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'API is running' });
@@ -17,7 +33,8 @@ app.get('/me', (req, res) => {
   res.status(200).json({
     name: 'Ayodeji Arib',   
     email: 'dejiwilliams9@gmail.com',     
-    github: 'https://github.com/deji2ghost'  
+    github: 'https://github.com/deji2ghost',
+    repo_name: "https://github.com/deji2ghost/personal-api"
   });
 });
 
